fcb1df7a3b67bd72e8f50aaa6dba2101b21b68ed
andre <ephiker@naver.com>
2020-03-22 02:20:42 +0900

    기능구현 완료

--- functions.js
+++ functions.js
@@ -1,15 +1,15 @@
-var index = 0;
-// index++;
 var itemArr = new Array();
 
-var TP_Post = function (param, idx) {
-    return `
-    <div id="DV_Thing">
-    <button id="BT_Thing" class="textField" onclick="Edit(id)">${param}</button>
-    <button id="BT_Delete,${idx}" onclick="DeleteThis(id)">DELETE</button>
-    </div>
-    `
+
+//포스트 함수는 입력된 스트링을 어레이에 추가해주고 리어레인지 함수를 호출한다. 
+function Post() {
+    itemArr.push(document.getElementById("IF_Todo").value);
+    document.getElementById("IF_Todo").value = "";
+    Rearrange();
 }
+
+
+//리어레인지 함수는 어레이에 있는 할 일 목록을 html에 작성한다. 
 function Rearrange() {
     var literal = "";
 
@@ -22,54 +22,62 @@ function Rearrange() {
 }
 
 
-function Post() {
-    itemArr.push(document.getElementById("IF_Todo").value);
-    document.getElementById("IF_Todo").value = "";
+var TP_Post = function (param, idx) {
+    return `
+    <div id="DV_Thing" data-idx="${idx}">
+    <button id="BT_Thing" data-idx="${idx}" class="textField" onclick="Edit(${idx})">${param}</button>
+    <button id="BT_Delete" data-idx="${idx}" onclick="DeleteThis(${idx})">DELETE</button>
+    </div>
+    `
+}
+
+
+function DeleteThis(targetIdx) {
+    itemArr.splice(targetIdx, 1);
     Rearrange();
 }
 
 
-function DeleteThis(buttonId) {
-    var parent = document.getElementById(buttonId).parentElement;
+function Edit(_idx) {
+    var txt_prev = itemArr[_idx];
+    var item = `
+    <input type="text" id="IP_Edit" data-idx="${_idx}" value="${txt_prev}" class="textField">
+    <button id="BT_Update" data-idx="${_idx}" onclick="Update(${_idx})" cols="44">UPDATE</button>
+    `
 
-    for (var i = 0; i < itemArr.length; i++) {
-        if (itemArr[i] == parent.firstElementChild.innerText) {
-            itemArr.splice(i, 1);
+    var thingsArr = document.querySelectorAll("#DV_Thing");
+    for (var i = 0; i < thingsArr.length; i++) {
+        if (thingsArr[i].dataset.idx == _idx) {
+            thingsArr[i].firstChild.remove();
+            thingsArr[i].firstChild.remove();
+            thingsArr[i].innerHTML = item;
         }
     }
-    Rearrange();
 }
 
+function Update(_idx) {
+    var new_text;
+    var inputArr = document.querySelectorAll("#IP_Edit");
+    for (var i = 0; i < inputArr.length; i++) {
+        if (inputArr[i].dataset.idx == _idx) {
+            new_text = inputArr[i].value //change text in array
+        }
+    }
 
-function Edit(elementId) {
-    var index_this = elementId.split(',')[1];
-    var txt_prev = document.getElementById(elementId).innerText;
-    var parent = document.getElementById(elementId).parentElement;
-    var item = `
-    <input type="text" value="${txt_prev}" class="textField">
-    <button id="BT_Update,${index_this}" onclick="Update(id)" cols="44">UPDATE</button>
-    `
-
-    parent.firstElementChild.remove();
-    parent.firstElementChild.remove();
-    parent.innerHTML = item;
-}
-
-
-
-function Update(elementId) {
-    var index_this = elementId.split(',')[1];
-    var parent = document.getElementById(elementId).parentElement;
-    var txt_prev = parent.firstElementChild.value;
-
+    itemArr[_idx] = new_text
 
     var item = `
-    <button id="BT_Thing,${index_this}" class="textField" onclick="Edit(id)">${txt_prev}</button>
-    <button id="BT_Delete,${index_this}" onclick="DeleteThis(id)">DELETE</button>
+    <button id="BT_Thing" class="textField" onclick="Edit(${_idx})">${new_text}</button>
+    <button id="BT_Delete" onclick="DeleteThis(${_idx})">DELETE</button>
     `
 
-    parent.firstElementChild.remove();
-    parent.firstElementChild.remove();
-    parent.innerHTML = item;
+    var thingsArr = document.querySelectorAll("#DV_Thing");
+    for (var i = 0; i < thingsArr.length; i++) {
+        if (thingsArr[i].dataset.idx == _idx) {
+            thingsArr[i].firstChild.remove();
+            thingsArr[i].firstChild.remove();
+            thingsArr[i].innerHTML = item;
+        }
+    }
 }
 
